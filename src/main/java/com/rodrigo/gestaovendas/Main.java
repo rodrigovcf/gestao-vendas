d039360e37eb0a77cb81111de88487c4e50a54e2
rodrigovcf <rodrigo.rvcf@gmail.com>
2025-03-17 14:30:51 -0300

    feat: ajuste no Main para executar Aplicacao

--- src/main/java/com/rodrigo/gestaovendas/Main.java
+++ src/main/java/com/rodrigo/gestaovendas/Main.java
@@ -1,13 +1,19 @@
 package com.rodrigo.gestaovendas;
 
-import com.rodrigo.gestaovendas.infra.ConexaoBD;
+import com.rodrigo.gestaovendas.app.ClienteService;
+import com.rodrigo.gestaovendas.domain.repositories.ClienteRepository;
+import com.rodrigo.gestaovendas.infra.ClienteDAO;
+import com.rodrigo.gestaovendas.ui.cliente.ClienteForm;
+
+import javax.swing.SwingUtilities;
 
 public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Testando conexão com o banco...");
-        ConexaoBD.testaConexao();
-
-	}
-
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            ClienteRepository clienteRepository = new ClienteDAO(); 
+            ClienteService clienteService = new ClienteService(clienteRepository);
+            new ClienteForm(clienteService);
+        });
+    }
 }
+
