6a424a125bc66a9b471efae4c87f56f683dd9bd1
rodrigovcf <rodrigo.rvcf@gmail.com>
2025-03-18 17:47:39 -0300

    feat: ajustando main para executar o menu criado

--- src/main/java/com/rodrigo/gestaovendas/Main.java
+++ src/main/java/com/rodrigo/gestaovendas/Main.java
@@ -1,19 +1,36 @@
 package com.rodrigo.gestaovendas;
 
 import com.rodrigo.gestaovendas.app.ClienteService;
+import com.rodrigo.gestaovendas.app.ProdutoService;
+import com.rodrigo.gestaovendas.app.VendaService;
 import com.rodrigo.gestaovendas.domain.repositories.ClienteRepository;
+import com.rodrigo.gestaovendas.domain.repositories.ProdutoRepository;
+import com.rodrigo.gestaovendas.domain.repositories.VendaRepository;
 import com.rodrigo.gestaovendas.infra.ClienteDAO;
-import com.rodrigo.gestaovendas.ui.cliente.ClienteForm;
-
-import javax.swing.SwingUtilities;
+import com.rodrigo.gestaovendas.infra.ProdutoDAO;
+import com.rodrigo.gestaovendas.infra.VendaDAO;
+import com.rodrigo.gestaovendas.ui.menu.MenuPrincipal;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            ClienteRepository clienteRepository = new ClienteDAO(); 
-            ClienteService clienteService = new ClienteService(clienteRepository);
-            new ClienteForm(clienteService);
-        });
+//        SwingUtilities.invokeLater(() -> {
+//            ClienteRepository clienteRepository = new ClienteDAO(); 
+//            ClienteService clienteService = new ClienteService(clienteRepository);
+//            new ClienteForm(clienteService);
+//        });
+    	
+    	
+    	ClienteRepository clienteRepository = new ClienteDAO();
+    	ClienteService clienteService = new ClienteService(clienteRepository);
+    	
+    	ProdutoRepository produtoRepository = new ProdutoDAO();
+    	ProdutoService produtoService = new ProdutoService(produtoRepository);
+
+    	VendaRepository vendaRepository = new VendaDAO();
+    	VendaService vendaService = new VendaService(vendaRepository, clienteRepository, produtoRepository);
+        
+        new MenuPrincipal(clienteService, produtoService, vendaService).setVisible(true);;
+
     }
 }
 
